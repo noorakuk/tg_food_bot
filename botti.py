242e01fc2cae421519039cef2aac0c6b02dd411d
Noora Kukkonen <noora.kukkonen@student.tuni.fi>
2020-05-04 13:51:56 +0300

    Adding items to the database works, getting the list does'n

--- botti.py
+++ botti.py
@@ -6,12 +6,29 @@ import configfile as config
 TOKEN  = config.TOKEN
 food_bot = telebot.TeleBot(token=TOKEN)
 
-# Needed functions
-def dbConnection(conn):
-    # DB connection
+creationQuery = """
+    CREATE TABLE IF NOT EXISTS groceries (
+        chatid NUMERIC NOT NULL,
+        item VARCHAR(20)  NOT NULL,
+        amount INTEGER
+        );
+    """
+conn = None
+try:
+    # Create connection
     conn = psycopg2.connect(host=config.HOST, database=config.DATABASE, user=config.USER, password=config.PASSWORD)
-    return conn
-
+    cur = conn.cursor()
+    # create table 
+    cur.execute(creationQuery)
+    # closes communication with the database server
+    cur.close()
+    # commit changes
+    conn.commit()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
 
 @food_bot.message_handler(commands=['start']) 
 def at_start(message):
@@ -20,33 +37,24 @@ def at_start(message):
         text = 'Hei ' + message.from_user.first_name + '!'
         food_bot.send_message(chatid, text)
 
-# Shoud add the new item to the db, table based on the chat id
-@food_bot.message_handler(commands=['newItem'])
-def add_new(message):
-    chatid = message.chat.id
-    print(message.content )
-    
+
+# Needed functions
+def dbConnection(conn):
+    # DB connection
+    conn = psycopg2.connect(host=config.HOST, database=config.DATABASE, user=config.USER, password=config.PASSWORD)
+    return conn
 
 
-# Create new table IF the chat is not in the db
-def createTableIfNotExcist():
-    creationQuery = """
-        CREATE TABLE IF NOT EXISTS groceries (
-            chatid NUMERIC NOT NULL,
-            item VARCHAR(20)  NOT NULL,
-            amount INTEGER
-            );
-        """
+def insertNewItem(chatid, item, amount):
+    sql = """INSERT INTO groceries(chatid, item, amount)
+             VALUES(%s, %s, %s)"""
     conn = None
     try:
-        # Create connection
         conn = dbConnection(conn)
         cur = conn.cursor()
-        # create table 
-        cur.execute(creationQuery)
-        # closes communication with the database server
+        cur.execute(sql, (str(chatid), item, str(amount)))
+
         cur.close()
-        # commit changes
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -54,7 +62,68 @@ def createTableIfNotExcist():
         if conn is not None:
             conn.close()
 
-createTableIfNotExcist()
+
+def getShoppingList(chatid):
+    sql = """SELECT 
+                item, amount 
+            FROM
+                groceries
+            WHERE
+                chatid = %s
+            ORDER BY item"""
+    conn = None
+    # result = ''
+    try:
+        conn = dbConnection(conn)
+        cur = conn.cursor()
+        
+        result = cur.execute(sql, (str(chatid)))
+        print(result)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def emptyShoppingList(chatid):
+    print('TODO')
+
+# Shoud add the new item to the db, table based on the chat id
+@food_bot.message_handler(commands=['newItem'])
+def add_new(message):
+    chatid = message.chat.id
+    words = message.text.split()
+    if len(words) == 3 or len(words) == 2:
+        # Check if the third argument is the number
+        amount = 1
+        if len(words) == 3:
+            try: 
+                amount = int(words[2])
+            except ValueError:
+                text = "Toinen parametri pitäisi olla kokonaisluku"
+                food_bot.send_message(chatid, text)
+                return
+        # Add item to the database
+        item = words[1]
+        insertNewItem(chatid, item, amount)
+        # Send confirm message
+        text = "Tuote " + item + " lisätty kauppalistaan"
+        food_bot.send_message(chatid, text)
+    else:
+        text = """Ensimmäinen sana on tuote, joka lisätään kauppalistaan. Jos haluat lisätä tuotteiden määrän, voit laittaa sen toiseksi parametriksi."""
+        food_bot.send_message(chatid, text)
+
+@food_bot.message_handler(commands=['getList'])
+def getItems(message):
+    chatid = message.chat.id
+    shoppingList = getShoppingList(chatid)
+    #print(shoppingList)
+
+
+# SHOULD BE END OF THE FILE BECAUSE OF THE PYTHON 
 # food_bot.set_update_listener(listener) #register listener
 food_bot.polling()
 #Use none_stop flag let polling will not stop when get new message occur error.
@@ -64,8 +133,3 @@ food_bot.polling(interval=3)
 
 while True: # Don't let the main Thread end.
     pass
-
-
-def insertNewItem(chatid, item, amount):
-    sql = """INSERT INTO groceries(chatid, item, amount)
-             VALUES(%s) RETURNING vendor_id;"""
