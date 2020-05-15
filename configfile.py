6cc8396786fcb1143bab457c9780ee7223beabfe
noorakuk <48064896+noorakuk@users.noreply.github.com>
2020-05-15 14:36:37 +0300

    Delete configfile.py

--- configfile.py
+++
@@ -1,7 +0,0 @@
-class Config:
-	def __init__(self):
-		self.TOKEN= '1126003161:AAGSmTTm6TA49bkiWrrKii_SyXXwEqA-Okg'
-		self.HOST='localhost'
-		self.DATABASE='tgbot'
-		self.USER='postgres'
-		self.PASSWORD='1234'
